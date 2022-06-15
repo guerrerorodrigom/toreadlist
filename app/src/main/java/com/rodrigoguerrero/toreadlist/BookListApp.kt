524ffb87639d8c807a56b405d2632851800fdede
Rodrigo Guerrero <rodrigo.guerreromulia@jumbo.com>
2022-06-15 20:07:58 +0200

    Changes.

--- app/src/main/java/com/rodrigoguerrero/toreadlist/BookListApp.kt
+++
@@ -1,7 +0,0 @@
-package com.rodrigoguerrero.toreadlist
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class BookListApp : Application()
--- app/src/main/java/com/rodrigoguerrero/toreadlist/network/models/SearchDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.rodrigoguerrero.toreadlist.network.models
-
-import com.google.gson.annotations.SerializedName
-
-data class SearchDTO(
-  @SerializedName("docs")
-  val books: List<BookDTO>
-)
